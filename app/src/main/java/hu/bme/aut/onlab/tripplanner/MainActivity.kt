8ff2ed6349f8a9c4b64b9143c8380668edb64c1d
Bailior0 <Balerion332>
2022-03-03 14:02:36 +0100

    Added Firebase authentication

--- app/src/main/java/hu/bme/aut/onlab/tripplanner/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/onlab/tripplanner/MainActivity.kt
@@ -1,33 +1,91 @@
 package hu.bme.aut.onlab.tripplanner
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.*
 import hu.bme.aut.onlab.tripplanner.databinding.ActivityMainBinding
 import hu.bme.aut.onlab.tripplanner.triplist.TriplistActivity
+import java.security.AccessController.getContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        FirebaseApp.initializeApp(this);
 
-        binding.btnSignIn.setOnClickListener {
-            when {
-                binding.etUsername.text.toString().isEmpty() -> {
-                    binding.etUsername.requestFocus()
-                    binding.etUsername.error = "Please enter your username"
-                }
-                binding.etPassword.text.toString().isEmpty() -> {
-                    binding.etPassword.requestFocus()
-                    binding.etPassword.error = "Please enter your password"
-                }
-                else -> {
-                    startActivity(Intent(this, TriplistActivity::class.java))
-                }
-            }
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener { registerClick() }
+        binding.btnLogin.setOnClickListener { loginClick() }
+    }
+
+    private fun validateForm(): Boolean {
+        if (binding.etEmail.text.isEmpty()) {
+            binding.etEmail.requestFocus()
+            binding.etEmail.error = "Please enter your email"
+            return false
         }
+        if (binding.etPassword.text.isEmpty()) {
+            binding.etPassword.requestFocus()
+            binding.etPassword.error = "Please enter your password"
+            return false
+        }
+        return true
+    }
+
+    private fun registerClick() {
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressDialog()
+
+        firebaseAuth
+            .createUserWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            .addOnSuccessListener { result ->
+                hideProgressDialog()
+
+                val firebaseUser = result.user
+                val profileChangeRequest = UserProfileChangeRequest.Builder()
+                    .setDisplayName(firebaseUser?.email?.substringBefore('@'))
+                    .build()
+                firebaseUser?.updateProfile(profileChangeRequest)
+
+                toast("Registration was successful")
+            }
+            .addOnFailureListener { exception ->
+                hideProgressDialog()
+
+                toast(exception.message)
+            }
+    }
+
+    private fun loginClick() {
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressDialog()
+
+        firebaseAuth
+            .signInWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            .addOnSuccessListener {
+                hideProgressDialog()
+
+                startActivity(Intent(this@MainActivity, TriplistActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { exception ->
+                hideProgressDialog()
+
+                toast(exception.localizedMessage)
+            }
     }
 }
