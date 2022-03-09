0e67bc907d87d90b21cca98fb31c76af7b2f8ebf
Bailior0 <Balerion332>
2022-03-09 19:42:05 +0100

    Icons reworked

--- app/src/main/java/hu/bme/aut/onlab/tripplanner/triplist/adapter/TriplistAdapter.kt
+++ app/src/main/java/hu/bme/aut/onlab/tripplanner/triplist/adapter/TriplistAdapter.kt
@@ -1,6 +1,7 @@
 package hu.bme.aut.onlab.tripplanner.triplist.adapter
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
